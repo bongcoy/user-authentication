382450cd310ece6c7dafda2834e3f3b060ccc7da
bongcoy <mpascaldewantara@gmail.com>
2024-05-13 16:07:33 +0700

    change to a list

--- models/post.js
+++ models/post.js
@@ -10,7 +10,7 @@ const postSchema = new mongoose.Schema({
     required: true,
   },
   categories: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: [mongoose.Schema.Types.ObjectId],
     ref: "Category",
     required: false,
   },
