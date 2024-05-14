919a839617ce6d6f59cecbbfea4c0515203adc48
bongcoy <mpascaldewantara@gmail.com>
2024-05-14 14:23:52 +0700

    add user validator

--- helpers/auth_validator.js
+++ helpers/auth_validator.js
@@ -1,5 +1,6 @@
 const {check} = require("express-validator");
 
+// Auth Validator
 exports.validateRegister = [
   check("name")
     .isLength({min: 3})
@@ -9,10 +10,17 @@ exports.validateRegister = [
     .isLength({min: 6})
     .withMessage("Password must be at least 6 characters long"),
 ];
-
 exports.validateLogin = [
   check("email").isEmail().withMessage("Email must be a valid email address"),
   check("password")
     .isLength({min: 6})
     .withMessage("Password must be at least 6 characters long"),
+];
+
+// User Validator
+exports.validateUserAdd = [
+  check("name")
+    .isLength({min: 3})
+    .withMessage("Name must be at least 3 characters long"),
+  check("email").isEmail().withMessage("Email must be a valid email address"),
 ];
